21e20071394df02f9ea3cc12b88c0ae7e46e5253
ravich43 <madu.ravichandran@mail.utoronto.ca>
2017-01-21 14:17:12 -0500

    started layout

--- IdeaGenerator/app/src/main/java/com/example/owner/ideagenerator/MainActivity.java
+++ IdeaGenerator/app/src/main/java/com/example/owner/ideagenerator/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.owner.ideagenerator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button searchButton = (Button)findViewById(R.id.searchButton);
+        final TextView newIdea = (TextView)findViewById(R.id.newIdea);
+        final EditText searchInput = (EditText)findViewById(R.id.searchEntry);
+        searchButton.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View view)
+                    {
+                        CharSequence searchField = searchInput.getText();
+                        RandomSentenceGenerator ideas = new RandomSentenceGenerator();
+                        CharSequence something = ideas.getIdeas(searchField);
+                        CharSequence newIdeas = ideas.generateIdea();
+                        newIdea.setText(newIdeas);
+                    }
+                }
+        );
+
+
     }
 }
